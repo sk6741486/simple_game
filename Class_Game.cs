d81d9b7ec429aa93c25e22036dee4df478a3f43d
Simranjit Kaur <sk6741486@gmail.com>
2020-11-13 15:10:22 +1300

    adding coment

--- Class_Game.cs
+++ Class_Game.cs
@@ -8,30 +8,30 @@ namespace simple_game
 {
    public class Class_Game
     {
-        public int load_position;
-        public int spin_position;
-        public int shoot_position;
-        public int chance=0;
-
+        public int load_position;//for data
+        public int spin_position;//for data
+        public int shoot_position;//for data
+        public int chance=0;//
+        
         public void load()
         {
             load_position = 1;
         }
-
+        // code load gun when click on load button
         public int spin()
         {
-            Random rnd_obj=new Random();
-            spin_position = rnd_obj.Next(1, 7);
+            Random rnd_obj=new Random();//for random chance
+            spin_position = rnd_obj.Next(1, 7);//for take chances
             return spin_position;
         }
-
-        public int shoot()
+        //spin the chamber of gun when click on spin button
+        public int shoot()//shoot code for shoot on head
         {
             if (load_position == spin_position)
             {
                 return 0;
             }
-            else if (spin_position == 6)
+            else if (spin_position == 6)//code for last chance of shoot
             {
                 spin_position = 1;
                 return 1;
@@ -39,7 +39,7 @@ namespace simple_game
             else
             {
                 spin_position++;
-                return 1;
+                return 1;//
              }
             }
         }          
